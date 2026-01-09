2453b23225ebda177fdee7859c7e88f05a8e777b
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2026-01-09 16:03:15 +0300

    Update arena.h

--- include/utils/arena.h
+++ include/utils/arena.h
@@ -20,7 +20,7 @@ typedef struct {
 } mem_arena_temp;
 
 mem_arena* arena_create(u64 reserve_size, u64 commit_size);
-void arena_destroy(mem_arena* arena);
+bool arena_destroy(mem_arena* arena);
 void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
 void arena_pop(mem_arena* arena, u64 size);
 void arena_pop_to(mem_arena* arena, u64 pos);
